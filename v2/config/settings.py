909d5ced614f25e21a2c964014d2eb0524c8c5ac
woshilsplbd <2481643536@qq.com>
2026-09-04 15:30:28 +0800

    feat: restore V2 FGO visual shell

--- v2/config/settings.py
+++ v2/config/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -136,6 +136,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
--- v2/config/urls.py
+++ v2/config/urls.py
@@ -2,8 +2,14 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    path(
+        "",
+        TemplateView.as_view(template_name="shell_preview.html"),
+        name="shell_preview",
+    ),
     path("admin/", admin.site.urls),
 ]
